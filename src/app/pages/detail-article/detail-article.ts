6ba5b2131b6493316aa10ef391121987013c3c58
Moustapha Diop <diopmoustapha351@gmail.com>
2026-09-13 21:46:14 +0000

    Configuration du routing et création des composants de pages

---
+++ src/app/pages/detail-article/detail-article.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-detail-article',
+  styleUrl: './detail-article.css',
+  templateUrl: './detail-article.html',
+})
+export class DetailArticle {}
